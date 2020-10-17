47e5987bbebee8081238f815cd87d8d3ef34c28e
Kirill Tolkun <tolkun_14@inbox.ru>
2020-10-17 23:40:14 +0300

    Add task 3

--- lab_3/lab_3_1.sql
+++ lab_3/lab_3_1.sql
@@ -10,3 +10,8 @@ SELECT *
 FROM DEPT
 WHERE DEPTADDR = 'CHICAGO'
    OR DEPTADDR = 'NEW YORK';
+
+-- 3. Найти минимальную заработную плату, начисленную в 2009 году (ФУНКЦИИ).
+SELECT MIN(SALVALUE)
+FROM SALARY
+WHERE YEAR = 2009;
