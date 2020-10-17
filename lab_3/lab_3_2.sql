8bdf4fdeeb614e9478c3c638de8579bef80f1a7d
Kirill Tolkun <tolkun_14@inbox.ru>
2020-10-17 23:50:22 +0300

    Add task 2

--- lab_3/lab_3_2.sql
+++ lab_3/lab_3_2.sql
@@ -8,3 +8,17 @@ FROM EMP
                         JOIN SALARY ON CAREER.EMPNO = SALARY.EMPNO
                WHERE SALVALUE = MINSALARY) SUBQUERY
               ON EMP.EMPNO = SUBQUERY.EMPNO;
+
+-- 2. Найти имена сотрудников, работавших или работающих в тех же отделах,
+-- в которых работал или работает сотрудник с именем RICHARD MARTIN (ПОДЗАПРОСЫ,
+-- ВОЗВРАЩАЮЩИЕ БОЛЕЕ ОДНОЙ СТРОКИ).
+SELECT EMPNAME
+FROM EMP
+         JOIN (SELECT DISTINCT CAREER.EMPNO
+               FROM CAREER
+                        JOIN (SELECT DEPTNO
+                              FROM CAREER
+                                       JOIN EMP ON CAREER.EMPNO = EMP.EMPNO
+                              WHERE EMPNAME = 'RICHARD MARTIN') DEPTNUMS
+                             ON CAREER.DEPTNO = DEPTNUMS.DEPTNO) EMPNUMS
+              ON EMP.EMPNO = EMPNUMS.EMPNO;
