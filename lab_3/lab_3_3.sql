3c2f3a0987a94bd58b7a53b5364d44ab80f50fa3
Kirill Tolkun <tolkun_14@inbox.ru>
2020-10-18 14:45:33 +0300

    Add task 2

--- lab_3/lab_3_3.sql
+++ lab_3/lab_3_3.sql
@@ -7,3 +7,30 @@ SELECT LEFT_EMP.EMPNO,
        LEFT_EMP.EMPNAME || ' works for ' || RIGHT_EMP.EMPNAME AS EMP_MANAGER
 FROM EMP LEFT_EMP
          JOIN EMP RIGHT_EMP ON LEFT_EMP.MANAGER_ID = RIGHT_EMP.EMPNO;
+
+-- 2. Требуется представить имя каждого сотрудника таблицы EMP
+-- (даже сотрудника, которому не назначен руководитель) и имя его
+-- руководителя (ИЕРАРХИЧЕСКИЙ (РЕКУРСИВНЫЙ) ЗАПРОС).
+
+-- Начинаем с корневого сотрудника (менеджера)
+-- (т.е. у которого 'MANAGER_ID' = NULL)
+SELECT LEVEL,
+       LEFT_EMP.EMPNO,
+       LEFT_EMP.EMPNAME,
+       LEFT_EMP.BIRTHDATE,
+       LEFT_EMP.EMPNAME || ' reports to ' || RIGHT_EMP.EMPNAME AS EMP_MANAGER
+FROM EMP LEFT_EMP
+         LEFT JOIN EMP RIGHT_EMP ON LEFT_EMP.MANAGER_ID = RIGHT_EMP.EMPNO
+START WITH LEFT_EMP.MANAGER_ID IS NULL
+CONNECT BY PRIOR LEFT_EMP.EMPNO = LEFT_EMP.MANAGER_ID;
+
+-- В виде дерева с сортировкой по имени внутри каждого уровня иерархии.
+-- У корневого элемента иерарахии MANAGER_ID = NULL, поэтому будет установлено
+-- 'NO'
+SELECT LPAD(' ', 3 * LEVEL) || LEVEL || '.' || EMPNAME || ' - ' ||
+       EMPNO || ' - ' || COALESCE(TO_CHAR(MANAGER_ID), 'NO') || ' - ' ||
+       BIRTHDATE AS TREE
+FROM EMP
+START WITH MANAGER_ID IS NULL
+CONNECT BY PRIOR EMPNO = MANAGER_ID
+ORDER SIBLINGS BY EMPNAME;
