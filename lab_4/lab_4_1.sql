ac528a1434aaf8c4046ca2472a241ee0fde923fb
Kirill Tolkun <tolkun_14@inbox.ru>
2020-11-15 20:10:41 +0300

    Add task 2

--- lab_4/lab_4_1.sql
+++ lab_4/lab_4_1.sql
@@ -3,3 +3,9 @@
 UPDATE JOB
 SET MINSALARY = 1000
 WHERE MINSALARY = (SELECT MIN(MINSALARY) FROM JOB);
+
+-- 2. Поднимите минимальную зарплату в таблице JOB на 10% для всех
+-- специальностей, кроме финансового директора.
+UPDATE JOB
+SET MINSALARY = MINSALARY * 0.1
+WHERE JOBNAME != 'FINANCIAL DIRECTOR';
